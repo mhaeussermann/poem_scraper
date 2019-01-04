825555a9e2844e21e88f861a2f3cb4a1d30b0558
Manuel H <mhaeussermann@users.noreply.github.com>
2019-01-04 17:47:10 +0100

    Changed code to scrape multiple pages

--- scrape_lyrikline.py
+++ scrape_lyrikline.py
@@ -3,14 +3,19 @@ from bs4 import BeautifulSoup
 import datetime
 import csv
 
-#end_page_num = 26
-
 lyrikline_de = "https://www.lyrikline.org/de/gedichte?query=&lang%5B%5D=de&listitems=100&page="
 
 page = requests.get(lyrikline_de)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#Get links to Lyrics
+# Iterate through pages
+def get_next_page_link(soup):
+    lis = soup.select('li.pager')
+    if lis:
+        return lis[0].find('a')
+    return None
+
+# Get links to Lyrpoemsics
 results = soup.find("ul", class_="liste clearfix").findAll("li", recursive=False)
 lyrik_links = []  
 for result in results:  
