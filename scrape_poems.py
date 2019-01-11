8eb0b2900d3c71decb497fd92a7f14822473a0ba
Manuel H <mhaeussermann@users.noreply.github.com>
2019-01-11 15:47:48 +0100

    Added method to sanitize file names.

--- scrape_poems.py
+++ scrape_poems.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pickle
 import os
-import csv
-
+import re
 
 # import poem url list
 with open ('poem_links.csv', 'rb') as fp:
@@ -35,7 +34,7 @@ for i in range(0, len(lyrik_links), 1):
 # extract title (h3 class="gedicht-origtitel-header")
     title = soup.find(class_='gedicht-origtitel-header')
     p_title = title.text
-    p_title = p_title.replace('/', '-')
+    p_clean_title = re.sub('[^a-zA-Z0-9 \n\.]', '', p_title)
     #print(p_title)
 # extract text of poem (div class="gedicht-originaltext clearfix ")
     poem_text = soup.find(class_='gedicht-originaltext clearfix')
@@ -44,7 +43,7 @@ for i in range(0, len(lyrik_links), 1):
     p_text = poem_text.text
     #print(p_text)
 
-    file_name = d2 + p_author + ' - ' + p_title + '.txt'
+    file_name = d2 + p_author + ' - ' + p_clean_title + '.txt'
     if not os.path.exists(file_name):
     # name file as 'author - title' and save poem text in file
         with open(file_name, 'w') as f:
